007518cb1f283004d9fc86e1714e0edf374d9ef1
Megapovelitel <megapovelitel@yandex.com>
2020-04-05 01:40:42 +0700

    Update comments.js

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 let Comment = require('../models/comment.model');
 
-router.route('/api/').get((req, res) => {
+router.route('/').get((req, res) => {
     Comment.find()
     .then(comments => res.json(comments))
     .catch(err => res.status(400).json('Error: ' + err));
