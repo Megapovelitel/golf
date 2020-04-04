fad034466a162fd266e544e1167e5415d8954cb3
Megapovelitel <megapovelitel@yandex.com>
2020-04-05 00:53:15 +0700

    Update comments.js

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -3,7 +3,7 @@ let Comment = require('../models/comment.model');
 
 router.route('/').get((req, res) => {
     Comment.find()
-    .then(comments => res.json(comments))
+    .then(alert('comment added'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
