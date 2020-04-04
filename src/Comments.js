342919b16903dcaeaad3697dc459ec3a77017c5b
Megapovelitel <megapovelitel@yandex.com>
2020-04-05 02:11:15 +0700

    q

--- src/Comments.js
+++ src/Comments.js
@@ -34,7 +34,7 @@ export default class Comments extends Component {
   }
 
    componentDidMount() {
-     axios.get('/comments-backend')
+     axios.get('/comments')
     .then (response => {
       this.setState({ isLoading: false, comments: response.data })
       console.log(this.state.comments)
@@ -47,7 +47,7 @@ export default class Comments extends Component {
  
   deleteComment(id) {
     
-    axios.delete('/comments-backend/'+id)
+    axios.delete('/comments/'+id)
     .then(res => console.log(res.data));
     this.setState({
       comments: this.state.comments.filter(el => el._id !== id)
