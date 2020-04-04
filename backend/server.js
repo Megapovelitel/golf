695d0cfa4b306f621a051ce7d45c87426a81f2f2
Megapovelitel <megapovelitel@yandex.com>
2020-04-04 12:10:55 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.redirect('/сomments');
 });
 
 const app = express();
