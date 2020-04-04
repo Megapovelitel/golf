88c9287aee133bb01c5df29d78b6b39f715d71a3
Megapovelitel <megapovelitel@yandex.com>
2020-04-04 21:43:28 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 
 
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
     );
     const connection = mongoose.connection;
