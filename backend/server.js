f9cd151a6b6b9f56f263a1c19dfcd13fd8b21da9
Megapovelitel <megapovelitel@yandex.com>
2020-04-05 00:01:52 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 5000;
 
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true}
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true}
     );
     const connection = mongoose.connection;
     connection.once('open', () => {
